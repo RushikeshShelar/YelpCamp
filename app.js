5387ac164392e34196dd21090ddcc7e7269a637a
Rushi <rushikeshshelar22222@gmail.com>
2023-09-09 18:35:00 +0530

    Moved Schemna Validation

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi');
-const methodOverride = require('method-override');
-
+const { campgroundSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-const { stat } = require("fs");
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
@@ -23,74 +21,75 @@ db.once("open", () => {
 
 const app = express();
 
-app.engine('ejs', ejsMate);
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
 
-app.get("/", (req, res) => {
-    res.render('home');
+app.get('/', (req, res) => {
+    res.render('home')
 });
-
-app.get("/campgrounds", catchAsync(async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
+    res.render('campgrounds/index', { campgrounds })
 }));
 
-app.get('/campgrounds/new', (req, res) => { 
+app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
-});
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-    const campgroundSchema = Joi.object({
-        campground: Joi.object({
-            title : Joi.string().required(),
-            price : Joi.number().required().min(0),
-        }).required(),
-    });
-    const result = campgroundSchema.validate(req.body);
-    console.log(result);
+})
+
+
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
 
 app.get('/campgrounds/:id', catchAsync(async (req, res,) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
 }));
 
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res,) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
-}));
+}))
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground} );
-    res.redirect(`/campgrounds/${campground._id}`);
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
 }));
 
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => { 
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }));
 
 app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404)); 
-});
+    next(new ExpressError('Page Not Found', 404))
+})
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500} = err;
-    if(!err.message) err.message = 'Something Went Wrong!';
-    res.status(statusCode).render('error', { err });
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
-  console.log("Server listening on port 3000");
-});
+    console.log('Serving on port 3000')
+})
