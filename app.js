d95b349e78b9cd36548b4a50ef268bd1482e1039
Rushi <rushikeshshelar.cs@gmail.com>
2023-09-28 21:12:41 +0530

    Configuring Session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 
@@ -31,6 +32,19 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'RushikeshShelar',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 *24 * 7,   
+        maxAge: 1000 * 60 * 60 *24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
