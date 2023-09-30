dfeea07e647b38e25fa6e2f4e9d7ef62cb310870
Rushi <rushikeshshelar.cs@gmail.com>
2023-09-30 23:46:21 +0530

    Review Permissions

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,13 +2,14 @@ const express = require('express');
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-const { validateReview } = require('../middleware.js');
+const { validateReview, isLoggedIn } = require('../middleware.js');
 const router = express.Router({ mergeParams: true });
 
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
