1e23c19973827e59190ca79284cc968b45fd1a6c
Rushi <rushikeshshelar.cs@gmail.com>
2023-09-30 13:09:16 +0530

    Add Basic Auth

--- middleware.js
+++ middleware.js
@@ -1,9 +1,17 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    req.session.returnTo = req.originalUrl;
     if (!req.isAuthenticated()) {
         req.flash('error', 'You must be signed in first');
         return res.redirect('/login');
     }
     next();
 }
+
+module.exports.storeReturnTo = (req, res, next) => {
+    if(req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+};
 
 
