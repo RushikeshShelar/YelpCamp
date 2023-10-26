23d00b4d45d17ab5e1debd73d95047d0d24fd98a
Rushi <rushikeshshelar.cs@gmail.com>
2023-10-26 20:21:09 +0530

    Displaying images as a Thumbnail

--- models/campground.js
+++ models/campground.js
@@ -2,15 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review');
 
+//https://res.cloudinary.com/ddxbv1j8u/image/upload/w_100/YelpCamp/ijlorfdm73u2rz9oy7hs.jpg
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 
 const CampgroundSchema = new Schema({ 
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
