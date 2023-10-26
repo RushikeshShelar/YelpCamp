0f1ce4efe9e0e6eb658a076c5752a6f5728276ae
Rushi <rushikeshshelar.cs@gmail.com>
2023-10-26 20:01:37 +0530

    Deleting images (backend)

--- controllers/campground.js
+++ controllers/campground.js
@@ -1,5 +1,5 @@
 const Campground = require('../models/campground');
-
+const cloudinary = require('cloudinary').v2;
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -49,7 +49,13 @@ module.exports.updateCampground = async (req, res) => {
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }))
     campground.images.push(...imgs);
-    campground.save();
+    await campground.save();
+    if(req.body.deleteImages){
+        for(let filename of req.body.deleteImages){
+           await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: {filename: { $in: req.body.deleteImages }}}});
+    }
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
