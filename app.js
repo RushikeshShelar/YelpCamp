672fd3bc607509a7f8355de9f6d39c39c58172af
Rushi <rushikeshshelar.cs@gmail.com>
2023-10-29 18:37:48 +0530

    Fixing Security Issues

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 const userRoutes = require('./routes/users');
 const campgroundsRoutes = require('./routes/campgrounds'); 
@@ -41,13 +43,16 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
+    name: 'aadi',
     secret: 'RushikeshShelar',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true, //only works on https
         expires: Date.now() + 1000 * 60 * 60 *24 * 7,   
         maxAge: 1000 * 60 * 60 *24 * 7
     }
@@ -55,6 +60,7 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 app.use(passport.initialize());
 app.use(passport.session());  
